a8896364e714eb2a473d1f110a7efdcd785fbebf
jaeseung.ko <jaeseung.ko@jasonko-mbp13.local>
2017-01-12 16:48:36 +0900

    Support loader for css module

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import styles from './Greeter.css';
 
 class Greeter extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         <h1>"Hi there and greetings"</h1>
       </div>
     );
